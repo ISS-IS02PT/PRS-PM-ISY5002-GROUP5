f7d9308742541abc7374544c8bc3dd1ac0eca42f
kenly-ldk <kenly.ldk@gmail.com>
2020-07-24 22:53:55 +0800

    flask_code.py

--- SystemCode/Deployment/deploy/flask_code.py
+++ SystemCode/Deployment/deploy/flask_code.py
@@ -31,10 +31,12 @@ def get_prediction():
     if request.method == 'POST':
         data = request.get_json()  # Get data posted as a json
         data = np.array(data)[np.newaxis, :]  # converts shape from (4,) to (1, 4)
+        data = scaler_load.transform(data)
         prediction = model_load.predict(data)  # runs globally loaded model on the data
     return str(prediction[0])
 
 
 if __name__ == '__main__':
+    load_scaler() # load scaler at the beginning once only
     load_model()  # load model at the beginning once only
     app.run(host='0.0.0.0', port=80)
